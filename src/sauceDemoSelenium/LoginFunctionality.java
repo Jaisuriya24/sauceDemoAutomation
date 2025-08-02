0c630a7890aca026be807f8c09189cb3fb45c635
Jaisuriya24 <jayasuriya018@gmail.com>
2025-08-02 14:47:52 +0530

    login function

---
+++ src/sauceDemoSelenium/LoginFunctionality.java
@@ -0,0 +1,11 @@
+package sauceDemoSelenium;
+
+public class LoginFunctionality {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.driver.chromedriver",
+				"C://Users//hp//Downloads//SeleniumHandsons//SeleniumFirstProject//chromedriver.exe");
+	}
+
+}
